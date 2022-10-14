0f145730a971ff2296ed80d22edeba7ce2ae46e0
Lucianogs90 <lucianogs90@gmail.com>
2022-10-14 16:16:53 -0400

    Criada a classe Pilha"

---
+++ FullStackException.java
@@ -0,0 +1,10 @@
+
+/**
+ * * Exceção de tempo de execução lançada quando alguém tenta executar uma
+ * operação top * ou pop sobre uma pilha vazia.
+ */
+public class FullStackException extends RuntimeException {
+    public FullStackException(String err) {
+        super(err);
+    }
+}
---
+++ Stack.java
@@ -0,0 +1,11 @@
+/** * Interface para uma pilha: uma coleção de objetos 
+ * que são inseridos e removidos de acordo com o princípio de o último que entra é o 
+ * primeiro que sai. Esta interface inclui os principais métodos de Java.util.Stack 
+ * 
+ * @author Roberto Tamassia 
+ * @author Michael Goodrich
+ **/
+
+public class Stack {
+    
+}
