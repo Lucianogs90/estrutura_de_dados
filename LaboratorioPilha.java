83faff84dc8aff33011ab5031d2dd18a58e1a357
Lucianogs90 <lucianogs90@gmail.com>
2022-10-14 19:01:17 -0400

    Tudo certo

--- LaboratorioPilha.java
+++ LaboratorioPilha.java
@@ -1,33 +1,38 @@
 public class LaboratorioPilha {
 
     public static void main(String[] args) {
-        
+
         String texto1 = "( )(( )){([( )])}";
         String texto2 = "((( )(( )){([( )])}))";
         String texto3 = ")(( )){([( )])}";
         String texto4 = "({[ ])}";
         String texto5 = "(";
         String texto6 = ")";
-        
+
         PilhaArray<String> pilha = new PilhaArray<String>();
 
-        String[] token = texto1.split("");
+        String[] token = texto6.split("");
 
         for (String caractere : token) {
-            if(caractere.matches("[\\)\\]\\}]") && pilha.size() == 0){
+            if (caractere.matches("[\\)\\]\\}]") && pilha.size() == 0) {
                 System.out.println("Tá errado");
-            } else if(caractere.matches("[\\(\\[\\{]")){
+                return;
+            } else if (caractere.matches("[\\(\\[\\{]")) {
                 pilha.push(caractere);
                 System.out.println(pilha);
-            } else if(caractere.matches("[\\)]") && pilha.top().matches("[\\(]")){
+            } else if (caractere.matches("[\\)]") && pilha.top().matches("[\\(]")
+                    || caractere.matches("[\\]]") && pilha.top().matches("[\\[]")
+                    || caractere.matches("[\\}]") && pilha.top().matches("[\\{]")) {
                 pilha.pop();
                 System.out.println(caractere);
                 System.out.println(pilha);
             }
         }
-        
-        
 
-
+        if(!pilha.isEmpty()){
+            System.out.println("Tá errado");
+        }else{
+            System.out.println("Tá certo");
+        }
     }
 }
