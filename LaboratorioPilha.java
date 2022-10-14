4a6906c8b0930e5a7babc38f78e022fcc1770970
Lucianogs90 <lucianogs90@gmail.com>
2022-10-14 19:10:49 -0400

    Método criado com sucesso

--- LaboratorioPilha.java
+++ LaboratorioPilha.java
@@ -1,5 +1,36 @@
 public class LaboratorioPilha {
 
+
+    public boolean symbolMatch(String texto){
+        PilhaArray<String> pilha = new PilhaArray<String>();
+
+        String[] token = texto.split("");
+
+        for (String caractere : token) {
+            if (caractere.matches("[\\)\\]\\}]") && pilha.size() == 0) {
+                /* System.out.println("Há problemas na abertura ou fechamento desta sequência"); */
+                return false;
+            } else if (caractere.matches("[\\(\\[\\{]")) {
+                pilha.push(caractere);
+                /* System.out.println(pilha); */
+            } else if (caractere.matches("[\\)]") && pilha.top().matches("[\\(]")
+                    || caractere.matches("[\\]]") && pilha.top().matches("[\\[]")
+                    || caractere.matches("[\\}]") && pilha.top().matches("[\\{]")) {
+                pilha.pop();
+                /* System.out.println(caractere);
+                System.out.println(pilha); */
+            }
+        }
+
+        if(!pilha.isEmpty()){
+            /* System.out.println("Há problemas na abertura ou fechamento desta sequência"); */
+            return false;
+        }else{
+            /* System.out.println("Tudo correto com a abertura e fechamento desta sequência"); */
+            return true;
+        }
+    }
+
     public static void main(String[] args) {
 
         String texto1 = "( )(( )){([( )])}";
@@ -9,30 +40,13 @@ public class LaboratorioPilha {
         String texto5 = "(";
         String texto6 = ")";
 
-        PilhaArray<String> pilha = new PilhaArray<String>();
+        LaboratorioPilha l = new LaboratorioPilha();
+        System.out.println(l.symbolMatch(texto1));
+        System.out.println(l.symbolMatch(texto2));
+        System.out.println(l.symbolMatch(texto3));
+        System.out.println(l.symbolMatch(texto4));
+        System.out.println(l.symbolMatch(texto5));
+        System.out.println(l.symbolMatch(texto6));
 
-        String[] token = texto6.split("");
-
-        for (String caractere : token) {
-            if (caractere.matches("[\\)\\]\\}]") && pilha.size() == 0) {
-                System.out.println("Há problemas na abertura ou fechamento desta sequência");
-                return;
-            } else if (caractere.matches("[\\(\\[\\{]")) {
-                pilha.push(caractere);
-                System.out.println(pilha);
-            } else if (caractere.matches("[\\)]") && pilha.top().matches("[\\(]")
-                    || caractere.matches("[\\]]") && pilha.top().matches("[\\[]")
-                    || caractere.matches("[\\}]") && pilha.top().matches("[\\{]")) {
-                pilha.pop();
-                System.out.println(caractere);
-                System.out.println(pilha);
-            }
-        }
-
-        if(!pilha.isEmpty()){
-            System.out.println("Há problemas na abertura ou fechamento desta sequência");
-        }else{
-            System.out.println("Tudo correto com a abertura e fechamento desta sequência");
-        }
     }
 }
