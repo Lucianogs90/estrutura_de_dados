196ff086f26a9feeac1cd6ff39c808dde9304e9c
Lucianogs90 <lucianogs90@gmail.com>
2022-10-14 18:13:46 -0400

    .

--- LaboratorioPilha.java
+++ LaboratorioPilha.java
@@ -7,18 +7,26 @@ public class LaboratorioPilha {
         String texto3 = ")(( )){([( )])}";
         String texto4 = "({[ ])}";
         String texto5 = "(";
+        String texto6 = ")";
         
         PilhaArray<String> pilha = new PilhaArray<String>();
 
         String[] token = texto1.split("");
 
         for (String caractere : token) {
-            if(caractere.matches("[\\)\\]\\}]") && pilha.top() == null){
+            if(caractere.matches("[\\)\\]\\}]") && pilha.size() == 0){
                 System.out.println("Tá errado");
-            } else if(caractere.matches())
+            } else if(caractere.matches("[\\(\\[\\{]")){
+                pilha.push(caractere);
+                System.out.println(pilha);
+            } else if(caractere.matches("[\\)]") && pilha.top().matches("[\\(]")){
+                pilha.pop();
+                System.out.println(caractere);
+                System.out.println(pilha);
+            }
         }
         
-        System.out.println(pilha.isEmpty());
+        
 
 
     }
