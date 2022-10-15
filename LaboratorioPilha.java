aedd1f87d07b1febb34585b0c15e7efd8e4eb9f0
Lucianogs90 <lucianogs90@gmail.com>
2022-10-15 16:32:59 -0400

    .

--- LaboratorioPilha.java
+++ LaboratorioPilha.java
@@ -1,33 +1,28 @@
 public class LaboratorioPilha {
 
-
-    public boolean symbolMatch(String texto){
+    public boolean symbolMatch(String texto) {
         PilhaArray<String> pilha = new PilhaArray<String>();
 
         String[] token = texto.split("");
 
         for (String caractere : token) {
             if (caractere.matches("[\\)\\]\\}]") && pilha.size() == 0) {
-                /* System.out.println("Há problemas na abertura ou fechamento desta sequência"); */
                 return false;
             } else if (caractere.matches("[\\(\\[\\{]")) {
                 pilha.push(caractere);
-                /* System.out.println(pilha); */
             } else if (caractere.matches("[\\)]") && pilha.top().matches("[\\(]")
                     || caractere.matches("[\\]]") && pilha.top().matches("[\\[]")
                     || caractere.matches("[\\}]") && pilha.top().matches("[\\{]")) {
                 pilha.pop();
-                /* System.out.println(caractere);
-                System.out.println(pilha); */
+            } else {
+                return false;
             }
         }
 
-        if(!pilha.isEmpty()){
-            /* System.out.println("Há problemas na abertura ou fechamento desta sequência"); */
-            return false;
-        }else{
-            /* System.out.println("Tudo correto com a abertura e fechamento desta sequência"); */
+        if (pilha.isEmpty()) {
             return true;
+        } else {
+            return false;
         }
     }
 
