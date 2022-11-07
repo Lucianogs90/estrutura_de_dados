1eb2d3d4b56250a7272a5519cbcd065607fb6ced
Lucianogs90 <lucianogs90@gmail.com>
2022-11-07 18:15:47 -0400

    Corrigidos os erros inesperados

--- filas/FilaArray.java
+++ filas/FilaArray.java
@@ -2,10 +2,11 @@ package filas;
 
 /**
  * Implementação da interface Fila usando um array de tamanho fixo.
- * Uma exceção é lançada ao tentar realizar uma operação de enqueue quando
- * o
- * tamanho da fila é igual ao tamanho do array. Esta classe inclui os
- * principais métodos de uma fila
+ * Uma exceção é lançada ao tentar realizar uma operação de enqueue quando o
+ * tamanho da fila é igual ao tamanho do array.
+ * Uma exceção é lançada ao tentar realizar uma operação de dequeue ou front 
+ * quando a fila estiver vazia.
+ * Esta classe inclui os principais métodos de uma fila
  */
 public class FilaArray<E> implements Fila<E> {
     protected int capacity; // capacidade real do array da fila
@@ -18,40 +19,48 @@ public class FilaArray<E> implements Fila<E> {
         this(CAPACITY);
     }
 
-    public FilaArray(int cap) {
-        this.capacity = cap;
-        this.S = (E[]) new Object[capacity]; // Está ok este aviso
-    }
-
-    public int size() {
-        return ((capacity - inicio + fim) % capacity);
+    public FilaArray(int capacity) {
+        this.capacity = capacity;
+        this.S = (E[]) new Object[this.capacity]; // Está ok este aviso
     }
 
     public boolean isEmpty() {
         return (inicio == fim);
     }
 
-    public void enqueue(E element) throws FullQueueException {
-        if (this.size() == capacity) {
-            throw new FullQueueException("A fila está cheia!");
-        }
-        S[fim++] = element;
+    public int size() {
+        return ((capacity - inicio + fim) % capacity);
     }
-
+    
     public E front() throws EmptyQueueException {
         if (isEmpty()) {
             throw new EmptyQueueException("A fila está vazia!");
         }
+        
         return S[inicio];
     }
-
+    
     public E dequeue() throws EmptyQueueException {
         if (isEmpty()) {
             throw new EmptyQueueException("A fila está vazia!");
         }
+        
         E element = S[inicio];
-        S[inicio++] = null; // exclui a referência ao elemnto que ocupava a posição top
+        
+        S[inicio] = null; // exclui a referência ao elemnto que ocupava a posição inicial
+        
+        inicio = (inicio + 1) % this.capacity; 
+        
         return element;
     }
+    
+    public void enqueue(E element) throws FullQueueException {
+        if (this.size() == capacity) {
+            throw new FullQueueException("A fila está cheia!");
+        }
 
+        S[fim] = element;
+
+        fim = (fim + 1) % this.capacity;
+    }
 }
--- filas/LaboratorioFila.java
+++ filas/LaboratorioFila.java
@@ -13,6 +13,29 @@ public class LaboratorioFila {
         System.out.println(fila.dequeue());
         System.out.println(fila.dequeue());
         System.out.println(fila.dequeue());
+
+        
+        FilaArray<Integer> fila2 = new FilaArray<Integer>(11);
+
+        for(int i = 0; i < 10; i++){
+            fila2.enqueue(i*i);
+        }
+
+        
+        for (int i = 0; i < 10; i++){
+            System.out.println(fila2.dequeue());
+        }
+
+        fila2.enqueue(100);
+        fila2.enqueue(200);
+        fila2.enqueue(300);
+        fila2.enqueue(400);
+
+        System.out.println(fila2.dequeue());
+        System.out.println(fila2.dequeue());
+        System.out.println(fila2.dequeue());
+        System.out.println(fila2.dequeue());
+
     }
 
 }
