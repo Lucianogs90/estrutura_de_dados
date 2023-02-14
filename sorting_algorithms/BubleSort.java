5a77b896cbf010f4b777aac6417a08e33a9d0228
Luciano Silva <lucianogs90@gmail.com>
2023-02-13 21:42:58 -0400

    criado o BubbleSort

---
+++ sorting_algorithms/BubleSort.java
@@ -0,0 +1,27 @@
+package sorting_algorithms;
+
+public class BubleSort {
+
+    public BubleSort(int[] numeros) {
+        int temp;
+        for (int i = 0; i < numeros.length; i++) {
+            for (int j = 0; j < numeros.length - 1; j++) {
+                if (numeros[j] > numeros[j + 1]) {
+                    temp = numeros[j];
+                    numeros[j] = numeros[j + 1];
+                    numeros[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numeros = { 1, 5, 3, 2, 4 };
+        
+        new BubleSort(numeros);
+
+    }
+}
